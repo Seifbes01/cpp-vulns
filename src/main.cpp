43df03b8eb1e2951cbefb58bd7ebf11716b670b0
Seifbes01 <mohamedseifeddine.besbes@insat.ucar.tn>
2025-06-10 10:29:19 +0200

    vulnerable cpp code

---
+++ src/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <cstring>
+#include <cstdio>
+
+void vulnerableBufferOverflow(const char* input) {
+    char buffer[10];
+    strcpy(buffer, input);  // Buffer overflow vulnerability
+    std::cout << "Buffer: " << buffer << std::endl;
+}
+
+void vulnerableFormatString(char* input) {
+    printf(input);  // Format string vulnerability
+}
+
+int main(int argc, char* argv[]) {
+    if (argc < 2) {
+        std::cout << "Usage: " << argv[0] << " <input>" << std::endl;
+        return 1;
+    }
+
+    vulnerableBufferOverflow(argv[1]);
+    vulnerableFormatString(argv[1]);
+
+    return 0;
+}
