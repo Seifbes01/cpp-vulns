26780343f2bf04abaf4400cdfb535ffc1945ebe4
Seifbes01 <mohamedseifeddine.besbes@insat.ucar.tn>
2025-06-10 11:12:03 +0200

    added vulns

--- src/main.cpp
+++ src/main.cpp
@@ -12,6 +12,16 @@ void vulnerableFormatString(char* input) {
     printf(input);  // Format string vulnerability
 }
 
+void vulnerableBufferOverflow(char* input) {
+    char buffer[10];
+    strcpy(buffer, input); // potential overflow
+}
+void useAfterFree() {
+    char* data = (char*)malloc(10);
+    free(data);
+    printf("%s", data);  // use after free
+}
+
 int main(int argc, char* argv[]) {
     if (argc < 2) {
         std::cout << "Usage: " << argv[0] << " <input>" << std::endl;
